d9d2a8742772355d0fdf0164e85c30908ca9c7d9
iwannnn <iwannnn1206@163.com>
2021-10-08 18:41:30 +0800

    home

--- blog-front-end/src/views/index.vue
+++ blog-front-end/src/views/index.vue
@@ -1,3 +1,38 @@
 <template>
     <div>hello world</div>
 </template>
+<script>
+import { listArticle } from "@/api/blog/article";
+export default {
+    name: "Article",
+    data() {
+        return {
+            articleList: [],
+            queryParams: {
+                pageNum: 1,
+                pageSize: 10,
+                userId: null,
+                tittle: null,
+                content: null,
+                musicId: null,
+                categoryId: null,
+                avatar: null,
+                summary: null,
+                pageviews: null,
+                likes: null,
+            },
+        };
+    },
+    created() {
+        this.getList();
+    },
+    methods: {
+        getList() {
+            listArticle(this.queryParams).then((res) => {
+                console.log(res);
+                this.articleList = res.rows;
+            });
+        },
+    },
+};
+</script>
