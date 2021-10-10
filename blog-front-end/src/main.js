d06061fcbc770dd21e80360cc9d15f41b267016e
iwannnn <iwannnn1206@163.com>
2021-10-10 19:27:33 +0800

    update framwork

--- blog-front-end/src/main.js
+++ blog-front-end/src/main.js
@@ -1,19 +1,40 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.prototype.msgSuccess = function (msg) {
+	this.$message({ showClose: true, message: msg, type: "success" });
+}
+
+Vue.prototype.msgError = function (msg) {
+	this.$message({ showClose: true, message: msg, type: "error" });
+}
+
+Vue.prototype.msgInfo = function (msg) {
+	this.$message.info(msg);
+}
+
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+
+Vue.use(Element, {
+	size: Cookies.get('size') || 'medium' // set element-ui default size
+})
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
-
-/* eslint-disable no-new */
 new Vue({
 	el: '#app',
 	router,
-	components: { App },
-	template: '<App/>'
+	render: h => h(App)
 })
--- blog-front-end/src/views/index.vue
+++ blog-front-end/src/views/index.vue
@@ -1,38 +1,33 @@
 <template>
-    <div>hello world</div>
+    <div>
+        <el-row :gutter="10">
+            <el-col :xs="0" :sm="5" :md="5" :lg="5" :xl="5">
+                <div class="grid-content bg-purple-light"></div>
+            </el-col>
+            <el-col :xs="24" :sm="14" :md="14" :lg="14" :xl="14">
+                hello world
+            </el-col>
+            <el-col :xs="0" :sm="5" :md="5" :lg="5" :xl="5">
+                <div class="grid-content bg-purple-light"></div>
+            </el-col>
+        </el-row>
+    </div>
 </template>
-<script>
-import { listArticle } from "@/api/blog/article";
-export default {
-    name: "Article",
-    data() {
-        return {
-            articleList: [],
-            queryParams: {
-                pageNum: 1,
-                pageSize: 10,
-                userId: null,
-                tittle: null,
-                content: null,
-                musicId: null,
-                categoryId: null,
-                avatar: null,
-                summary: null,
-                pageviews: null,
-                likes: null,
-            },
-        };
-    },
-    created() {
-        this.getList();
-    },
-    methods: {
-        getList() {
-            listArticle(this.queryParams).then((res) => {
-                console.log(res);
-                this.articleList = res.rows;
-            });
-        },
-    },
-};
-</script>
+<style>
+.el-col {
+    border-radius: 4px;
+}
+.bg-purple-dark {
+    background: #99a9bf;
+}
+.bg-purple {
+    background: #d3dce6;
+}
+.bg-purple-light {
+    background: #e5e9f2;
+}
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+</style>
