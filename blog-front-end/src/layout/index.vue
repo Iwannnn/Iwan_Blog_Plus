eded1f08746d3fd1068d1d9e69de928c16011f65
iwannnn <iwannnn1206@163.com>
2021-10-10 19:27:16 +0800

    layout

---
+++ blog-front-end/src/layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>
+                <div>
+                    <keep-alive><router-view /></keep-alive>
+                </div>
+            </el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Layout",
+};
+</script>
+
+<style>
+.el-header,
+.el-footer {
+    background-color: #b3c0d1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-main {
+    background-color: #e9eef3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+.el-col {
+    border-radius: 4px;
+}
+.bg-purple-dark {
+    background: #99a9bf;
+}
+.bg-purple {
+    background: #d3dce6;
+}
+.bg-purple-light {
+    background: #e5e9f2;
+}
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+</style>
